fa0a71ebbf9fb73e858bef9ac72c33261456cac6
Akuku <wanyama19@gmail.com>
2020-11-06 22:29:46 +0300

    createUser method added to handle addition of new users

--- src/main/java/com/instatech/controllers/users/UsersController.java
+++ src/main/java/com/instatech/controllers/users/UsersController.java
@@ -1,9 +1,31 @@
 package com.instatech.controllers.users;
 
+import com.instatech.dto.userdto.UserDto;
+import com.instatech.models.charges.requests.UserRequestModel;
+import com.instatech.models.charges.responses.UserResponse;
+import com.instatech.services.user.UserService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users")
 public class UsersController {
+    @Autowired
+    UserService userService;
+
+    @PostMapping
+    public UserResponse createUSer(@RequestBody UserRequestModel userDetails) {
+        UserResponse returnValue = new UserResponse();
+
+        UserDto userDto = new UserDto();
+        BeanUtils.copyProperties(userDetails, userDto);
+        UserDto createdUser = userService.createUser(userDto);
+
+        BeanUtils.copyProperties(createdUser, returnValue);
+        return returnValue;
+    }
 }
